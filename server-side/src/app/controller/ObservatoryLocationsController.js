2f2e05998c29615843c66f1a2417ae3464fe5b25
Luis Rondow <luiseduardorondow@gmail.com>
2019-10-19 20:03:43 -0300

    add route locations

--- server-side/src/app/controller/ObservatoryLocationsController.js
+++ server-side/src/app/controller/ObservatoryLocationsController.js
@@ -2,7 +2,18 @@
 import ObservatoryLocations from "../models/ObservatoryLocations";
 
 class ObservatoryLocationsController {
-  
+    async index(req, res) {
+        const observatories = await ObservatoryLocations.find();
+
+        return res.json(observatories);
+    }
+
+    async show(req, res) {
+        const { symbol } = req.params;
+        const observatory = await ObservatoryLocations.findOne(symbol);
+
+        return res.json(observatory);
+    }
 }
 
 export default new ObservatoryLocationsController();
