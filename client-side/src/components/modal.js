f768059950ce89deb7367a44782fb8df6561181a
andre <andrersfcostagmail.com>
2019-10-20 11:17:15 -0300

    pombin daqnp

--- client-side/src/components/modal.js
+++ client-side/src/components/modal.js
@@ -16,6 +16,7 @@ const Modal = ({ handleClose, showItem, children, items }) => {
     const Card = styled.section`
         position:fixed;
         display: flex;
+        flex-direction: column;
         background: white;
         width: 80%;
         height: 50%;
@@ -24,25 +25,45 @@ const Modal = ({ handleClose, showItem, children, items }) => {
         border-radius: 14px;
         padding: 20px 15px;
         transform: translate(-50%,-50%);
+        overflowY: scroll;
     `;
 
     const Button = styled.button`
         align-self: flex-end;
-        width: 100%;
-        margin: 0 50px;
+        width: 400px;
+        margin: 0 auto;
         outline: none;
         padding:10px 15px;
         border: 1px solid black;
         border-radius: 20px;
         background-color: white;
         color: black;
+        margin-top: 20px 0;
+    `;
+
+    const Title = styled.h2`
+        color: black;
+        font-size: 20px;
+        margin: 0 auto;
+    `;
+
+    const Description = styled.p`
+        color: black;
+        font-size: 12px;
+        margin: 0 auto;
     `;
 
      return showItem ?  
         <Overlay>
-            {console.log(items)}
           <Card>
             {children}
+            {console.log(items ? items.properties[0] : null)}
+            <Title>{items? items.name : "Sem nome explicito"}</Title>
+            {console.log(items)}
+            {items && items.properties[0] ?
+            <Description>{items.properties[0].description}</Description> :
+            <Description>"Sem descricao aparente"</Description>
+            }
             <Button onClick={handleClose}>Fechar modal</Button>
           </Card>
         </Overlay>
