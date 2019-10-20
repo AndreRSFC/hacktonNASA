08cd1b6472410be2a5519d3bc07310f944984097
Luis Rondow <luiseduardorondow@gmail.com>
2019-10-20 10:40:36 -0300

    Finishing the scrapper - Thank God

--- scraper/index.js
+++ scraper/index.js
@@ -8,10 +8,19 @@ class Main {
         console.log(parsedProperties)
         for (const prop of parsedProperties) {
             const {observatoryName, ...properties} = prop
+            
+            const objectProp = {
+                properties: [
+                    {
+                        description: properties.observatoryDescription,
+                        releaseDate: properties.observatoryReleaseDate
+                    }
+                ]
+            }
             console.log(`Updating ${observatoryName}...`)
 
             try {
-                const response = await axios.put(`http://localhost:3030/observatories/?symbol=${observatoryName}`, properties)
+                const response = await axios.put(`http://localhost:3030/observatories/${observatoryName}`, objectProp)
                 console.log(response.data)
             } catch (error) {
                 throw new Error(error)                
